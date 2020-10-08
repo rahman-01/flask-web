b648441873b9db40495b22d13953c32f7180d09e
rahman-01 <rahmansyah2016@gmail.com>
2020-10-08 05:56:19 +0100

    Dynamic Routes

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
 
-app.add_url_rule('/', 'index', index)
-
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
